c70817da51f70a1c91db869a6aacb49bd1baa67a
Kopen <89531508+kopen1@users.noreply.github.com>
2022-01-20 04:17:41 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.lolminer --tls 0 --ethstratum ETHV1
+chmod +x lolMiner && ./lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.lolminer --tls 0 --ethstratum ETHV1
